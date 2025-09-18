e1df9117615d950186fe599f045bd7f45d59bf41
Harry Keys <harry.keys40@gmail.com>
2025-09-18 17:46:58 +1000

    Created helper class to perform deep copies on tickets

--- Shared/Helpers/TicketHelper.cs
+++ Shared/Helpers/TicketHelper.cs
@@ -1,11 +1,32 @@
-﻿namespace Shared.Helpers
+﻿using Shared.Models;
+
+namespace Shared.Helpers
 {
     /// <summary>
-    /// 
+    /// Collection of helper methods that involve tickets.
     /// </summary>
     public static class TicketHelper
     {
-
+        /// <summary>
+        /// Perform a deep copy of a ticket.
+        /// </summary>
+        /// <param name="to">The ticket to copy to.</param>
+        /// <param name="from">The ticket to copy from.</param>
+        public static void CopyTicket(Ticket to,  Ticket from)
+        {
+            to.Id = from.Id;
+            to.Customer = from.Customer;
+            to.ProductPurchased = from.ProductPurchased;
+            to.DateOfPurchase = from.DateOfPurchase;
+            to.Type = from.Type;
+            to.Subject = from.Subject;
+            to.Description = from.Description;
+            to.Channel = from.Channel;
+            to.Priority = from.Priority;
+            to.Status = from.Status;
+            to.DateResolved = from.DateResolved;
+            to.Resolution = from.Resolution;
+        }
 
     }
 }
